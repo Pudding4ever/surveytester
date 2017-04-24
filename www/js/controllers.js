9a54a0554bda90584a77c2cdb3c2b1708f865242
Pudding4ever <dkerrcresswell@gmail.com>
2017-04-24 04:16:49 -0400

    or maybe now

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', [])
   
-.controller('homeLoginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeLoginCtrl', ['$scope', '$stateParams','$rootScope', '$firebaseAuth', '$window', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
@@ -48,12 +48,43 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+.controller('signupCtrl', [
+    '$scope', '$rootScope', '$firebaseAuth', '$window',
+    function ($scope, $rootScope, $firebaseAuth, $window) {
+      $scope.user = {
+        email: "",
+        password: ""
+      };
+      $scope.createUser = function () {
+        var email = this.user.email;
+        var password = this.user.password;
 
+        if (!email || !password) {
+          $rootScope.notify("Please enter valid credentials");
+          return false;
+        }
 
+        $rootScope.show('Please wait.. Registering');
+        $rootScope.auth.$createUser(email, password, function (error, user) {
+          if (!error) {
+            $rootScope.hide();
+            $rootScope.userEmail = user.email;
+            $window.location.href = ('#/page12');
+          }
+          else {
+            $rootScope.hide();
+            if (error.code == 'INVALID_EMAIL') {
+              $rootScope.notify('Invalid Email Address');
+            }
+            else if (error.code == 'EMAIL_TAKEN') {
+              $rootScope.notify('Email Address already taken');
+            }
+            else {
+              $rootScope.notify('Oops something went wrong. Please try again later');
+            }
+          }
+        });
+      }
 }])
    
 .controller('createSurveyCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
